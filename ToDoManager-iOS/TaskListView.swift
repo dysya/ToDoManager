3336e7e6f4b8399a70fd87f5708906f7e385812d
Daniil Akifev <akifev.ru@gmail.com>
2022-08-09 10:47:52 +0300

    Fix top spacing

--- ToDoManager-iOS/TaskListView.swift
+++ ToDoManager-iOS/TaskListView.swift
@@ -12,40 +12,38 @@ struct TaskListView: View {
     @Binding var project: Project
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                List {
-                    ForEach (project.tasks) { task in
-                        Text(task.description)
-                    }
-                    .onDelete(perform: removeItems)
+        ZStack {
+            List {
+                ForEach (project.tasks) { task in
+                    Text(task.description)
                 }
-                .navigationTitle(project.name)
-                VStack {
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle(project.name)
+            VStack {
+                Spacer()
+                HStack {
                     Spacer()
-                    HStack {
-                        Spacer()
-                        Button(
-                            action: {
-                                project.tasks.append(Task(description: "New Task"))
-                            },
-                            label: {
-                                Text("+")
-                                .font(.system(.largeTitle))
-                                .frame(width: 57, height: 50)
-                                .foregroundColor(Color.white)
-                                .padding(.bottom, 7)
-                            }
-                        )
-                        .background(Color.blue)
-                        .cornerRadius(38.5)
-                        .padding()
-                        .shadow(color: Color.black.opacity(0.3),
-                            radius: 3,
-                            x: 3,
-                            y: 3
-                        )
-                    }
+                    Button(
+                        action: {
+                            project.tasks.append(Task(description: "New Task"))
+                        },
+                        label: {
+                            Text("+")
+                            .font(.system(.largeTitle))
+                            .frame(width: 57, height: 50)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 7)
+                        }
+                    )
+                    .background(Color.blue)
+                    .cornerRadius(38.5)
+                    .padding()
+                    .shadow(color: Color.black.opacity(0.3),
+                        radius: 3,
+                        x: 3,
+                        y: 3
+                    )
                 }
             }
         }
